55a73952ba93358494d7c536be80e0588d2a7846
Cheng.Liu <cheng.liu@geneegroup.com>
2018-10-05 00:56:43 +0800

    短信功能完善

--- class/Gini/Controller/CGI/AJAX/Login.php
+++ class/Gini/Controller/CGI/AJAX/Login.php
@@ -26,13 +26,25 @@ class Login extends \Gini\Controller\CGI
         $code->save();
 
         // 发送短信码
-        $http = new \Gini\HTTP();
-        $r = $http->request('get', 'http://api.smsbao.com/sms', [
-            'u' => 'jianbing',
-            'p' => md5('23155212'),
-            'm' => $code->phone,
-            'c' => urlencode(strtr("【煎饼财富】您正在注册煎饼财富，验证码%code（切勿泄露！），有效期5分钟，任何人向您索要，或要求您打开网页输入验证码均是诈骗行为。如非本人操作，请致电4009618893。", ['%code' => $code->identity]))
-        ]);
+        $statusStr = array(
+            "0" => "短信发送成功",
+            "-1" => "参数不全",
+            "-2" => "服务器空间不支持,请确认支持curl或者fsocket，联系您的空间商解决或者更换空间！",
+            "30" => "密码错误",
+            "40" => "账号不存在",
+            "41" => "余额不足",
+            "42" => "帐户已过期",
+            "43" => "IP地址限制",
+            "50" => "内容含有敏感词"
+        );
+
+        $smsapi = "http://api.smsbao.com/";
+        $user = "jianbing"; // 短信平台帐号
+        $pass = md5("23155212"); //短信平台密码
+        $content = strtr("【煎饼财富】您正在注册煎饼财富，验证码%code（切勿泄露！），有效期5分钟，任何人向您索要，或要求您打开网页输入验证码均是诈骗行为。如非本人操作，请致电4009618893。", ['%code' => $code->identity]);//要发送的短信内容
+        $phone = $code->phone; // 要发送短信的手机号码
+        $sendurl = $smsapi."sms?u=".$user."&p=".$pass."&m=".$phone."&c=".urlencode($content);
+        $result = file_get_contents($sendurl);
         
         return \Gini\IoC::construct('\Gini\CGI\Response\JSON', ['success' => true]);
     }
